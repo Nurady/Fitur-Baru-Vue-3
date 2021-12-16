778f89eda5d5144e96306eb24cf6f401ae5cd770
unknown <nursyafriady83@gmail.com>
2021-12-16 17:57:21 +0800

    Suspense

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Suspense>
+        <template #default>
+            <div>
+                <Users />
+                    <hr>
+                <Posts />
+            </div>
+        </template>
+        <template #fallback>
+            <div>Loading . . .</div>
+        </template>
+    </Suspense>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Users from '@/components/Users'
+import Posts from '@/components/Posts'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default{
+    components: { Users, Posts },      
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-for="post in posts.data" :key="post.id">
+        <div>
+            {{ post.title }}
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default{
+        async setup () {
+            const posts = await axios.get('https://jsonplaceholder.typicode.com/posts')
+
+            return {
+                posts
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>

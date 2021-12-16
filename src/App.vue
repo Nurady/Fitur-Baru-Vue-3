b8f5f9462212f8060a4c3b7488d0c5168c62535f
unknown <nursyafriady83@gmail.com>
2021-12-16 21:04:27 +0800

    Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <Suspense>
+    <!-- <Suspense>
         <template #default>
             <div>
                 <Users />
@@ -16,16 +16,42 @@
         <teleport to="#modal">
             <Modal />
         </teleport>
+    </div> -->
+    <div v-for="user in users" :key="user.id">
+        <div>{{ user.name }}</div>
     </div>
 </template>
 
 <script>
-import Users from '@/components/Users'
-import Posts from '@/components/Posts'
-import Modal from '@/components/Modal'
+import axios from 'axios'
+import { onMounted, reactive, toRefs } from 'vue'
+// import Users from '@/components/Users'
+// import Posts from '@/components/Posts'
+// import Modal from '@/components/Modal'
 
-export default{
-    components: { Users, Posts, Modal },      
+export default {
+    // components: { Users, Posts, Modal },   
+    setup () {
+        // const users = ref(null)
+        const state = reactive({
+            users: []
+        })
+
+        const getUsers = async () => {
+            let {data} = await axios.get('https://jsonplaceholder.typicode.com/users')
+            state.users = data
+        }
+
+        onMounted(() => {
+            getUsers()
+        }) 
+
+        return {
+            // state
+            ...toRefs(state)
+            // users
+        }
+    }
 }
 </script>
 

2fa4ad3ae54ce47ffac651c28e97d3bde191979b
unknown <nursyafriady83@gmail.com>
2021-12-16 18:39:06 +0800

    Teleport

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,21 @@
             <div>Loading . . .</div>
         </template>
     </Suspense>  
+    <hr>
+    <div>
+        <teleport to="#modal">
+            <Modal />
+        </teleport>
+    </div>
 </template>
 
 <script>
 import Users from '@/components/Users'
 import Posts from '@/components/Posts'
+import Modal from '@/components/Modal'
 
 export default{
-    components: { Users, Posts },      
+    components: { Users, Posts, Modal },      
 }
 </script>
 
---
+++ src/components/Modal.vue
@@ -0,0 +1,62 @@
+<template>
+    <button @click="modalOpen = true">
+        Show Modal
+    </button>
+    <div v-if="modalOpen" class="modal-overlay">
+        <div class="modal">
+            <div class="modal-header">
+                this is header
+            </div>
+            <div class="modal-body">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, tempore nihil omnis neque dolores voluptatum totam sunt, porro aspernatur est quis provident eum nisi eius rem cumque commodi perferendis amet?
+            </div>
+            <div class="modal-footer">
+                <button @click="modalOpen = false">Close</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            modalOpen:false,
+
+        }
+    }
+}
+</script>
+
+<style>
+    .modal-overlay {
+       position: absolute;
+       top: 0;
+       bottom: 0;
+       left: 0;
+       right: 0;
+       background: rgba(0, 0, 0, 0.5);
+       content: "";
+    }
+
+    .modal {
+        border: 1px solid #eee;
+        background: white;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .modal-header {
+        border-bottom: 1px solid #eee
+    }
+
+    .modal-footer {
+        border-top: 1px solid #eee
+    }
+
+    .modal-body, .modal-header, .modal-footer {
+        padding: 10px
+    }
+</style>
